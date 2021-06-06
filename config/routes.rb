31b3b1c685e57900ebc4ab258d4b5952adeea8fc
papi-gorilla <jindeensadayoshi@gmail.com>
2021-06-06 07:43:41 +0000

    [add] ルーティングの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "homes#top"
+  get "/about" => "homes#about"
+  get "/tutorial" => "homes#tutorial"
+  resources :users, only:[:show, :edit, :update] do
+    member do
+      get :following, :followed
+    end
+  end
+  post "follow/:id" => "relationships#create", as: "follow"
+  delete "unfollow/:id" => "relationships#destroy", as: "unfollow"
+  resources :posts, only:[:new, :create, :show, :edit, :update, :destroy] do
+    resources :comments, only:[:create, :destroy]
+    resource :favorites, only:[:create, :index]
+  end
+  resources :searches, only:[:new, :index]
 end
