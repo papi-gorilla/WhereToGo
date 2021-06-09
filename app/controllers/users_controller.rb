abdca8962fb50caca7e279b6f7644a070a1c9406
papi-gorilla <jindeensadayoshi@gmail.com>
2021-06-09 16:48:45 +0000

    [add] ユーザー編集機能のストロングパラメーターを追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,11 +10,13 @@ class UsersController < ApplicationController
 
   def update
     @user = User.find(params[:id])
-    @user.save
+    @user.save(user_params)
     redirect_to user_path(@user)
   end
 
   private
-
+　def user_params
+　  params.require(:user).permit(:user_image, :user_name, :introduction)
+　end
 
 end
