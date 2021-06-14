50ae353a06181f6f53c85d8d68317795fb88f60a
papi-gorilla <jindeensadayoshi@gmail.com>
2021-06-14 22:50:20 +0000

    [add] フォロー機能を追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,8 +13,8 @@ class UsersController < ApplicationController
     @user.update(user_params)
     redirect_to user_path(@user)
   end
-
-private
+  
+  private
   def user_params
     params.require(:user).permit(:user_image, :name, :user_name, :email, :introduction)
   end
--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,3 +1,5 @@
 class Relationship < ApplicationRecord
-  
+  belongs_to :following, class_name: "User"
+  belongs_to :followed, class_name: "User"
+
 end
