f5d70d33067ed7b52f5113a3d662b3d70e7cb250
papi-gorilla <jindeensadayoshi@gmail.com>
2021-06-23 03:33:36 +0000

    [fix] 投稿詳細ページのレイアウトを修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -13,6 +13,10 @@ class CommentsController < ApplicationController
   end
 
   def destroy
+    @post = Post.find(params[:id])
+    @comment = current_user.comments.find_by(post_id: @post.id)
+    @comment.destroy
+    redirect_to post_path(@post)
   end
 
   private
