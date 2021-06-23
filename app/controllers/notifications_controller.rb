83d8ccf30283f70981b470d2bf44c23758cd548c
papi-gorilla <jindeensadayoshi@gmail.com>
2021-06-23 08:36:48 +0000

    [fix] 通知一覧ページのレイアウトを追加

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -3,7 +3,7 @@ class NotificationsController < ApplicationController
 
   def index
     @notifications = current_user.passive_notifications
-    @notifications.where(checked: false).each do |notification|
+    @notifications.where(checked: false).limit(10).each do |notification|
       notification.update_attributes(checked: true)
     end
   end
