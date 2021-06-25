21422b0deac1c5901a58a7e951aa0e99ffdc8041
papi-gorilla <jindeensadayoshi@gmail.com>
2021-06-25 12:15:41 +0000

    [fix] 各種ページをレスポンシブ対応に修正

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "posts#index"
   get "/about" => "homes#about"
-  get "/tutorial" => "homes#tutorial"
   resources :users, only:[:show, :edit, :update] do
     member do
       get :following, :followed
