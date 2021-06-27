31914e8dcd78da26d8b66fbe222373437bd75d66
papi-gorilla <jindeensadayoshi@gmail.com>
2021-06-27 14:52:12 +0000

    [add] commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do |n|
+  User.create!(
+    email: "test#{n+1}@test.com",
+    user_name: "test君#{n+1}",
+    password: "#{n+1}#{n+1}#{n+1}#{n+1}#{n+1}#{n+1}"
+  )
+end
+
+User.all.each do |user|
+  user.posts.create!(
+    title: "山陰旅行一周旅行！"
+  )
+end
