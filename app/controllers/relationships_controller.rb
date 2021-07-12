4f1067902d79d576e9ca3b2aa5d5477ec34c5542
papi-gorilla <jindeensadayoshi@gmail.com>
2021-07-12 19:16:25 +0000

    [add] フォロー機能に非同期処理を追加

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,15 +1,33 @@
 class RelationshipsController < ApplicationController
   def follow
+    @user = User.find(params[:id])
     user = User.find(params[:id])
     current_user.follow(params[:id])
     user.create_notification_follow!(current_user)
-    redirect_to user_path(user)
+    path = Rails.application.routes.recognize_path(request.referer)
+    if path[:controller] == "users" && path[:actions] == "following"
+      render templete: "users/user_index"
+    elsif path[:controller] == "users" && path[:actions] == "followed"
+      render templete: "users/user_index"
+    elsif path[:controller] == "searches" && path[:actions] == "index"
+      render templete: "users/user_index"
+    elsif path[:controller] == "users" && path[:actions] == "show"
+      render templete: "users/user_follow"
+    end
   end
 
   def unfollow
+    @user = User.find(params[:id])
     user = User.find(params[:id])
     current_user.unfollow(params[:id])
-    redirect_to user_path(user)
+    path = Rails.application.routes.recognize_path(request.referer)
+    if path[:controller] == "users" && path[:actions] == "following"
+      render templete: "users/user_index"
+    elsif path[:controller] == "users" && path[:actions] == "followed"
+      render templete: "users/user_index"
+    elsif path[:controller] == "searches" && path[:actions] == "index"
+      render templete: "users/user_index"
+    end
   end
 
 end
